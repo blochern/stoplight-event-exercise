f9c86384ff033ab3f8b8ab92c4d1a566ee928370
Nathanael Blocher <blocher.nathanael@gmail.com>
2023-12-14 11:36:02 -0800

    Completed bonus assignment

--- index.js
+++ index.js
@@ -1,50 +1,20 @@
-(function() {
+(function () {
   'use strict';
 
-  // YOUR CODE HERE
-
-  // query the document object for the stop button and its corresponding light
-  const stopButton = document.querySelector('#stopButton');
-  const stopLight = document.querySelector('#stopLight')
-  // add a click event listener for the stop button
-  stopButton.addEventListener('click', (e) => {
-    // toggle the stop light's 'stop' class to change its color to red
-    stopLight.classList.toggle('stop');
-  });
-
-  // query the DOM for the slow button/light
-  const slowButton = document.querySelector('#slowButton');
-  const slowLight = document.querySelector('#slowLight');
-  // add a click event listener for the slow button
-  slowButton.addEventListener('click', (e) => {
-    // toggle the slow light's 'slow' class to change its color to yellow
-    slowLight.classList.toggle('slow');
-  });
-
-  // go button/light
-  const goButton = document.querySelector('#goButton');
-  const goLight = document.querySelector('#goLight');
-  goButton.addEventListener('click', (e) => {
-    goLight.classList.toggle('go');
-  });
-
-  // handler function for the 'enter' event
-  const handleEnter = (e) => {
-    console.log(`Mouse entered the ${e.target.textContent} button`);
+  // event handler function
+  const eventHandler = (e) => {
+    // filter for only buttons
+    if (!e.target.classList.contains('button')) { return; }
+    // grab the button's name by splitting the id with "B"
+    const name = e.target.id.split("B")[0];
+    // now use the name to access its corresponding light
+    const light = document.querySelector(`#${name}Light`);
+    // if the light's classList contains its name (if it's already on), log 'off' -- if not, log 'on'
+    console.log(`${e.target.textContent} bulb ${light.classList.contains(name) ? `off` : `on`}`);
+    // toggle the class list (to turn the bulb off or on)
+    light.classList.toggle(name);
   }
 
-  // handler function for the 'leave' event
-  const handleLeave = (e) => {
-    console.log(`Mouse left the ${e.target.textContent} button`);
-  }
-
-  // query the dom for all items with a class of '.button'
-  const buttonList = document.querySelectorAll('.button');
-  // loop over this list, adding event listeners to each of them
-  for (let button of buttonList) {
-    button.addEventListener('mouseenter', handleEnter);
-    button.addEventListener('mouseleave', handleLeave);
-  }
-
-
+  // add a DOM event listener for the window
+  document.addEventListener('click', eventHandler);
 })();
