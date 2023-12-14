6e30f1b4626f5591e95ae6691c45df7a06ee6d14
Nathanael Blocher <blocher.nathanael@gmail.com>
2023-12-14 13:08:18 -0800

    Removed whitespace and comments

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 (function () {
   'use strict';
 
-  // event handler function
   const eventHandler = (e) => {
-    // filter for only buttons
     if (!e.target.classList.contains('button')) { return; }
-    // grab the button's name by splitting the id with "B"
     const name = e.target.id.split("B")[0];
-    // now use the name to access its corresponding light
     const light = document.querySelector(`#${name}Light`);
-    // if the light's classList contains its name (if it's already on), log 'off' -- if not, log 'on'
     console.log(`${e.target.textContent} bulb ${light.classList.contains(name) ? `off` : `on`}`);
-    // toggle the class list (to turn the bulb off or on)
     light.classList.toggle(name);
   }
 
-  // add a DOM event listener for the window
   document.addEventListener('click', eventHandler);
 })();
